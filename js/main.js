5a0d816274dc7789fc7682ec5a3e358fec030578
Yuriy_log <ylogachev-sfedu@mail.ru>
2020-11-10 23:23:27 +0300

    Added dots instead arrows. Footer adaptive DONE

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-$(function(){
+$(function () {
 
     $('.slider__inner, .news__slider-inner').slick({
-        nextArrow:'<button type="button" class="slick-btn slick-next"></button>',
-        prevArrow:'<button type="button" class="slick-btn slick-prev"></button>',
-        infinite: false
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 900,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
     });
 
     $('select').styler();
 
-    $('.header__btn-menu').on('click', function(){
+    $('.header__btn-menu').on('click', function () {
         $('.menu ul').slideToggle();
     });
 
